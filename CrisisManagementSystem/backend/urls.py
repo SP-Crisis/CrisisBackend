bd373c76789fd2d53ef97f60d756c779f13bac38
theSlimeChive <jalengilyard99@gmail.com>
2021-02-06 05:55:54 +0000

    Stopping point 1am

--- CrisisManagementSystem/backend/urls.py
+++ CrisisManagementSystem/backend/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [ 
     path('', views.index, name="Home"),
     path('dashboard/', views.dashboard, name="Dashboard"),
+    path('questions/<int:question_id>/', views.questions, name="QList"),
+    path('questions/', views.allQuestions, name="All Questions"),
     path('search/', views.search, name="Search"),
     path("rank/", views.rank, name="Ranking"),
 ]
--- CrisisManagementSystem/backend/views.py
+++ CrisisManagementSystem/backend/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
 
 # Create your views here.
 def index(request):
@@ -7,6 +9,15 @@ def index(request):
 
 def dashboard(request): 
     return HttpResponse("Dashboard")
+
+def allQuestions(request): 
+    questions = Question.objects.all()
+    return HttpResponse(questions)
+
+def questions(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    
+    return HttpResponse(question.question)
 
 def search(request):
     return HttpResponse("Search")
