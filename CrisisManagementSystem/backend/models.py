ad7d5388c557e9962df69e24b2109baba166ff74
Evan <ciriel@live.com>
2021-02-07 19:12:28 -0500

    Added answer model, will want to add more to Q/A once users are figured out

--- CrisisManagementSystem/backend/models.py
+++ CrisisManagementSystem/backend/models.py
@@ -7,3 +7,10 @@ class Question(models.Model):
     question = models.CharField(max_length=500)
     def __str__(self):
         return self.question
+
+class Answers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=500)
+    rank = models.IntegerField(default=0)
+    def __str__(self):
+        return self.answer
