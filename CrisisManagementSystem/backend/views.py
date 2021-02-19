5695406538228764efbfe63608bc1fe3af5cd642
knottyChive <jalengilyard99@gmail.com>
2021-02-19 13:22:33 -0500

    API Router

--- CrisisManagementSystem/backend/views.py
+++ CrisisManagementSystem/backend/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question, Answers
-from .serializers import QuestionSerializer
+from .serializers import QuestionSerializer, AnswerSerializer
 
 from rest_framework import viewsets
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    
+
+class AnswerViewSet(viewsets.ModelViewSet):
+    queryset = Answers.objects.all()
+    serializer_class = AnswerSerializer
