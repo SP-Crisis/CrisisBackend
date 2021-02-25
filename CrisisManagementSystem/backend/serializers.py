ca28822fa6deb5e21962b910b124abe06eb70c2f
Evan <ciriel@live.com>
2021-02-25 17:56:20 -0500

    User Serializer

--- CrisisManagementSystem/backend/serializers.py
+++ CrisisManagementSystem/backend/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from .models import Question, Answers
+from .models import Question, Answers, Policies, Technologies, Operations, Health, User
 
 #Serializers
 ## Main Serial
 
 class QuestionSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Question
         fields = '__all__'
 
@@ -33,3 +33,13 @@ class HealthSerializer(serializers.ModelSerializer):
     class Meta:
         model = Health
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+#Django's user model has the following fields:
+#username, first_name, last_name, email, password, groups,
+#user_permissions, is_staff, is_active, is_superuser, last_login, date_joined
+#We can discuss which fields to use, I just left it as all for now to test
