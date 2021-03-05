db7a64665f2d9c9fd78e25d5a11a85dc0e0430ce
Evan <ciriel@live.com>
2021-03-05 16:57:24 -0500

    Login user story

--- CrisisManagementSystem/backend/models.py
+++ CrisisManagementSystem/backend/models.py
@@ -27,7 +27,6 @@ class Policies(models.Model):
 class Technologies(models.Model):
     types = models.CharField(max_length=10)
     technology = models.CharField(max_length=500)
-    type = models.CharField(blank=True, options = types.choices, max_length=10)
     def __str__(self):
         return self.technology
 
