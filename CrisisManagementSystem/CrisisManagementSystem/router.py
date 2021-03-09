19015f1d75a7419b38f0489227c3a803b8f93c80
knottyChive <jalengilyard99@gmail.com>
2021-03-09 17:16:19 -0500

    Jalen: Working deploy with DB

--- CrisisManagementSystem/CrisisManagementSystem/router.py
+++ CrisisManagementSystem/CrisisManagementSystem/router.py
@@ -2,6 +2,7 @@ from backend.views import QuestionViewSet, AnswerViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('questions', QuestionViewSet, basename='question')
-router.register('answers', AnswerViewSet, basename="answer")
+router.register('questions', QuestionViewSet)
+router.register('answers', AnswerViewSet)
 
+urlpatterns = router.urls
