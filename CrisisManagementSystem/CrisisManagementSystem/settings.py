31d8253fb042a6b56e553ff302efc4af95103cf2
knottyChive <jalengilyard99@gmail.com>
2021-03-09 23:13:36 -0500

    Attempt4

--- CrisisManagementSystem/CrisisManagementSystem/settings.py
+++ CrisisManagementSystem/CrisisManagementSystem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -156,3 +156,4 @@ DJOSER = {
         'user_delete': 'djoser.serializers.UserDeleteSerializer',
     }
     }
+django_heroku.settings(locales())
